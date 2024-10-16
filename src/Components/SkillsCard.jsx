7346e32c57131550d6bbc2a51f970eb36f5fce9e
Gobinda Gagan <gobindagagan@gmail.com>
2024-10-16 20:31:53 +0530

    work with the skill  card

--- src/Components/SkillsCard.jsx
+++ src/Components/SkillsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SkillsCard = ({item, category}) => {
   return (
     <div className="p-3 flex flex-wrap">
-    <div className="  bg-[#1F1F1F] rounded-lg  w-[300px] max-h-[400px] h-[300px] border border-gray-500 shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] ">
+    <div className="  bg-[#1F1F1F] rounded-lg  w-[300px] max-h-[400px] h-full md:h-[300px] border border-gray-500 shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] ">
       <p className="font-semibold text-2xl text-center py-2 text-[#f0f1f0]">
         {item.category}
       </p>
