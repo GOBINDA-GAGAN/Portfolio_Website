f5d123f5f6b442a5e3a4727216d5eb7fc4ca8aa2
Gobinda Gagan <gobindagagan@gmail.com>
2024-10-16 20:28:44 +0530

    done with skill

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = ({title,subtitle}) => {
   return (
     <div>
-      <div className="w-8/12 mx-auto">
+      <div className="md:w-9/12 w-full mx-auto p-3">
         <h1 className="text-4xl font-bold  text-[#06B6D4]">{title}</h1>
         <p className="pt-2 pb-2 px-1 text-gray-300">
           {subtitle}
