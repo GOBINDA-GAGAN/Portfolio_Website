733f2f42a262fec8ae6bc5732af6e12d07c61e2d
Gobinda Gagan <gobindagagan@gmail.com>
2024-10-16 21:25:58 +0530

    #

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,11 +8,6 @@ import logo from "/logo5.png";
 const Navbar = () => {
   const NavBarData = [
     {
-      name: "Gobinda Gagan",
-      link: "/", // Home link
-      image: logo,
-    },
-    {
       name: "About",
       link: "/about", // Absolute path
     },
@@ -34,29 +29,19 @@ const Navbar = () => {
   const [theme, setTheme] = useState(false);
 
   return (
-    <nav className="font-custom bg-[#000000] px-10 py-5 overflow-x-hidden">
-      <div className="w-8/12 mx-auto text-white flex justify-between items-center">
-        <ul className="flex items-center px-3 py-2 gap-7 border-[#06B6D4] border bg-[#27272A] rounded-2xl">
+    <nav className="font-custom bg-[#000000] px-10 py-5 ">
+      <div className="md:w-8/12  mx-auto text-white flex justify-between items-center">
+        <ul className="md:flex items-center px-3 py-2 gap-7 border-[#06B6D4] border bg-[#27272A] rounded-2xl  ">
+          <div className="flex gap-2 items-center">
+            <img src={logo} className="h-8 w-8 rounded-full object-cover object-top" />
+            <span className="font-semibold">Alok</span>
+          </div>
+
           {NavBarData.map((value, index) => {
             return (
-              <li key={index}>
-                <Link to={value.link}>
-                  <div
-                    className={`flex items-center gap-3 hover:bg-slate-600 px-1 rounded-lg ${
-                      value.name === "Gobinda Gagan"
-                        ? "font-semibold"
-                        : "font-normal"
-                    }`}
-                  >
-                    {value.image && (
-                      <img
-                        src={logo}
-                        alt={value.name}
-                        className="h-8 w-8 rounded-full object-cover"
-                      />
-                    )}
-                    {value.name}
-                  </div>
+              <li key={index} className="hidden md:visible">
+                <Link to={value.link} className="font-medium">
+                  <span>{value.name}</span>
                 </Link>
               </li>
             );
